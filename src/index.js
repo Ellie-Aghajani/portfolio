dbc7e61b3aa3985094e2c0012ba8b100a8010543
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-25 17:03:04 -0700

    apply theme in index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./theme"; //
+import CssBaseline from "@mui/material/CssBaseline";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline /> {/* Ensures consistent baseline styles */}
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
