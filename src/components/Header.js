192863d1d7f59906a840e68c07c90381f309d869
Ellie-Aghajani <z.aqajani@gmail.com>
2024-08-25 17:09:27 -0700

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+// src/components/Header.js
+import React from "react";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          My Portfolio
+        </Typography>
+        <Button color="inherit" component={Link} to="/">
+          About
+        </Button>
+        <Button color="inherit" component={Link} to="/projects">
+          Projects
+        </Button>
+        <Button color="inherit" component={Link} to="/contact">
+          Contact
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
